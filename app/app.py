4153c814a72585f17fc3f12849b60f08532f4e97
finesebastian <snf5@njit.edu>
2021-12-02 13:13:41 -0500

    Unifying version names

--- app/app.py
+++ app/app.py
@@ -1,7 +1,6 @@
 """A simple flask web app"""
 from flask import Flask, request
 from flask import render_template
-from calc.calculator import Calculator
 app = Flask(__name__)
 
 @app.route("/home")
